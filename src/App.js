3edfad62f3911843445031ce5f5d54cfff0a9bd2
rmaqlctnekf <rmaqlctnekf@naver.com>
2020-09-30 12:49:13 +0900

    made component event

--- src/App.js
+++ src/App.js
@@ -9,24 +9,69 @@ class App extends Component {
         super(props);
 
         this.state = {
+            mode: 'read',
+            selected_content_id: 2,
             subject: { title: 'WEB', sub: 'world wide web' },
+            welcome: { title: 'Welcome', desc: 'Hello, React!!'},
             contents: [
-                {id:1, title: 'HTML', desc: 'HTML is HyperText ...'},
+                {id:1, title: 'HTML', desc: 'HTML is for information'},
                 {id:2, title: 'CSS', desc: 'CSS is for design'},
-                {id:3, title: 'JavaScript', desc: 'JavaScript is interactive ...'}
+                {id:3, title: 'JavaScript', desc: 'JavaScript is interactive'}
             ]
         }
     }
 
     render() {
+        console.log('App render')
+        let _title, _desc = null;
+
+        if(this.state.mode === 'welcome') {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if (this.state.mode === 'read') {
+            // let data = this.state.contents[this.state.selected_content_id - 1];
+            
+            // _title = data.title;
+            // _desc = data.desc;
+
+            let i = 0;
+
+            while (i < this.state.contents.length) {
+                let data = this.state.contents[i];
+                if(data.id === this.state.selected_content_id) {
+                    _title = data.title
+                    _desc = data.desc
+                    break;
+                }
+
+                i += 1;
+            }
+        }
+
         return (
             <div className="App">
                 <Subject 
                     title={this.state.subject.title} 
-                    sub={this.state.subject.sub}>
+                    sub={this.state.subject.sub}
+                    onChangePage={() => {
+                        this.setState({
+                            mode: 'welcome'
+                        })
+                    }}
+                    >
                 </Subject>
-                <NavBar data={this.state.contents}></NavBar>
-                <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+                <NavBar 
+                    data={this.state.contents}
+                    onChangePage = {(id) => {
+                        this.setState({
+                            mode: 'read',
+                            selected_content_id: Number(id)
+                        })                        
+                    }}
+                >
+                    
+                </NavBar>
+                <Content title={_title} desc={_desc}></Content>
             </div>
         );
     }
--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -7,7 +7,18 @@ class NavBar extends Component {
         let i = 0;
 
         while(i < data.length) {
-            lists.push(<li key={data[i].id}><a href={"/content/" + data[i].id + ".html"}>{data[i].title}</a></li> )
+            lists.push(
+                <li key={data[i].id}>
+                    <a onClick={(e)=> {
+                        e.preventDefault();  //이벤트를 막는다.
+                        this.props.onChangePage(e.target.dataset.id);
+                    }}  href={"/content/" + data[i].id}
+                        data-id={data[i].id}
+                    > 
+                        {data[i].title} 
+                    </a>
+                </li> 
+            )
             i += 1;
         }
         return (
