d5a4de31cb6584deb3dc9496e73db359316af514
rmaqlctnekf <rmaqlctnekf@naver.com>
2020-09-30 23:35:50 +0900

    modify App Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Subject from './components/views/Subject/Subject'
+import CRUD from './components/views/CRUD/CrudManage'
 import NavBar from './components/views/NavBar/NavBar'
-import Content from './components/views/Content/Content'
+import CreateContent from './components/views/CRUD/CreateContent'
+import ReadContent from './components/views/CRUD/ReadContent';
 import './App.css';
 
 class App extends Component {
@@ -10,27 +12,29 @@ class App extends Component {
 
         this.state = {
             mode: 'read',
-            selected_content_id: 2,
+            selected_content_id: 1,
             subject: { title: 'WEB', sub: 'world wide web' },
-            welcome: { title: 'Welcome', desc: 'Hello, React!!'},
+            welcome: { title: 'Welcome', desc: 'Hello, React!!' },
             contents: [
-                {id:1, title: 'HTML', desc: 'HTML is for information'},
-                {id:2, title: 'CSS', desc: 'CSS is for design'},
-                {id:3, title: 'JavaScript', desc: 'JavaScript is interactive'}
+                { id: 1, title: 'HTML', desc: 'HTML is for information' },
+                { id: 2, title: 'CSS', desc: 'CSS is for design' },
+                { id: 3, title: 'JavaScript', desc: 'JavaScript is interactive' }
             ]
         }
     }
 
     render() {
-        console.log('App render')
-        let _title, _desc = null;
+        console.log('render app');
 
-        if(this.state.mode === 'welcome') {
+        let _title, _desc = null;
+        let crud_element = null;
+
+        if (this.state.mode === 'welcome') {
             _title = this.state.welcome.title;
             _desc = this.state.welcome.desc;
         } else if (this.state.mode === 'read') {
             // let data = this.state.contents[this.state.selected_content_id - 1];
-            
+
             // _title = data.title;
             // _desc = data.desc;
 
@@ -38,9 +42,9 @@ class App extends Component {
 
             while (i < this.state.contents.length) {
                 let data = this.state.contents[i];
-                if(data.id === this.state.selected_content_id) {
-                    _title = data.title
-                    _desc = data.desc
+                if (data.id === this.state.selected_content_id) {
+                    _title = data.title;
+                    _desc = data.desc;
                     break;
                 }
 
@@ -48,30 +52,63 @@ class App extends Component {
             }
         }
 
+        switch (this.state.mode) {
+            case "create":
+                crud_element = <CreateContent onCreate={(data) => {
+                    let _content = this.state.contents.concat({
+                        id: this.state.contents.length + 1,
+                        title: data.title,
+                        desc: data.desc
+                    });
+                    
+                    this.setState({ contents: _content })
+                }}></CreateContent>;
+                break;
+            
+            case "welcome":
+            case "read":
+                crud_element = <ReadContent title={_title} desc={_desc}></ReadContent>;
+                break;
+
+            case "update":
+                break;
+
+            case "delete":
+                break;
+            default:
+                break;
+        }
+
         return (
             <div className="App">
-                <Subject 
-                    title={this.state.subject.title} 
+                <Subject
+                    title={this.state.subject.title}
                     sub={this.state.subject.sub}
                     onChangePage={() => {
                         this.setState({
                             mode: 'welcome'
                         })
                     }}
-                    >
+                >
                 </Subject>
-                <NavBar 
+                <CRUD onChangeMode={(nMode) => {
+                    this.setState({
+                        mode: nMode
+                    })
+                }}
+                ></CRUD>
+                <NavBar
                     data={this.state.contents}
-                    onChangePage = {(id) => {
+                    onChangePage={(id) => {
                         this.setState({
                             mode: 'read',
                             selected_content_id: Number(id)
-                        })                        
+                        })
                     }}
                 >
-                    
                 </NavBar>
-                <Content title={_title} desc={_desc}></Content>
+
+                {crud_element}
             </div>
         );
     }
